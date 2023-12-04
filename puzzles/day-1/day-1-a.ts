de687b35bb693722cebcddf7cb3e79b00a5b6d25
Abdelmounaim Belghalem <abelghalem@greenspector.com>
2023-12-04 11:32:32 +0100

    feat(day1): add code for day 1

--- puzzles/day-1/day-1-a.ts
+++ puzzles/day-1/day-1-a.ts
@@ -1,9 +1,40 @@
-import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import { readData } from '../../shared.ts';
+
+function isNumber(value: string) {
+  return ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].includes(value);
+}
+
+function getFirstNumber(value: string) {
+  for (let i = 0; i < value.length; i++) {
+    if (isNumber(value.at(i))) {
+      return value.at(i);
+    }
+  }
+
+  throw new Error('No number');
+}
+
+function getLastNumber(value: string) {
+  for (let i = 1; i <= value.length; i++) {
+    if (isNumber(value.at(-i))) {
+      return value.at(-i);
+    }
+  }
+
+  throw new Error('No number');
+}
 
 export async function day1a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let total = 0;
+
+  for (const value of data) {
+    total += parseInt(`${getFirstNumber(value)}${getLastNumber(value)}`);
+  }
+
+  return total;
 }
 
 const answer = await day1a();
--- puzzles/day-1/day-1-b.ts
+++ puzzles/day-1/day-1-b.ts
@@ -1,10 +1,85 @@
-import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import { readData } from '../../shared.ts';
 
-export async function day1b(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+const numbers = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '0',
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+  'zero',
+];
+
+const numbersMap = {
+  '1': 1,
+  '2': 2,
+  '3': 3,
+  '4': 4,
+  '5': 5,
+  '6': 6,
+  '7': 7,
+  '8': 8,
+  '9': 9,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
+function getFirstNumber(value: string) {
+  for (let i = 0; i < value.length; i++) {
+    for (const number of numbers) {
+      if (value.startsWith(number, i)) {
+        return numbersMap[number];
+      }
+    }
+  }
+
+  throw new Error('No number');
 }
 
-const answer = await day1b();
+function getLastNumber(value: string) {
+  for (let i = 0; i < value.length; i++) {
+    for (const number of numbers) {
+      if (value.endsWith(number, value.length - i)) {
+        return numbersMap[number];
+      }
+    }
+  }
+
+  throw new Error('No number');
+}
+
+export async function day1a(dataPath?: string) {
+  const data = await readData(dataPath);
+
+  let total = 0;
+
+  for (const value of data) {
+    total += parseInt(`${getFirstNumber(value)}${getLastNumber(value)}`);
+  }
+
+  return total;
+}
+
+const answer = await day1a();
 console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
