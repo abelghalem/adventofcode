fe75559e1baa547cabb57ab02b1a6e7b01c9f4fa
Abdelmounaim Belghalem <abelghalem@greenspector.com>
2023-12-06 23:39:31 +0100

    feat(day2): add code for day 2

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -1,9 +1,48 @@
-import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import { readData } from '../../shared.ts';
+
+const redCubes = 12;
+const greenCubes = 13;
+const blueCubes = 14;
 
 export async function day2a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let total = 0;
+
+  for (const game of data) {
+    const maxCubes = {
+      red: 0,
+      green: 0,
+      blue: 0,
+    };
+
+    const gameId = parseInt(game.split(':')[0].split(' ')[1]);
+    const parts = game.slice(game.indexOf(': ') + 2).split(';');
+    for (const part of parts) {
+      const cubePulls = part.split(', ');
+
+      for (const cubePull of cubePulls) {
+        const cubePullParts = cubePull.trim().split(' ');
+        const number = parseInt(cubePullParts[0]);
+        const color = cubePullParts[1];
+
+        if (maxCubes[color] === -1 || maxCubes[color] < number) {
+          maxCubes[color] = number;
+        }
+      }
+    }
+
+    if (
+      maxCubes.red <= redCubes &&
+      maxCubes.green <= greenCubes &&
+      maxCubes.blue <= blueCubes
+    ) {
+      total += gameId;
+    }
+  }
+
+  return total;
 }
 
 const answer = await day2a();
--- puzzles/day-2/day-2-b.ts
+++ puzzles/day-2/day-2-b.ts
@@ -1,9 +1,46 @@
-import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import { readData } from '../../shared.ts';
+
+const redCubes = 12;
+const greenCubes = 13;
+const blueCubes = 14;
 
 export async function day2b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let total = 0;
+
+  for (const game of data) {
+    const minCubes = {
+      red: -1,
+      green: -1,
+      blue: -1,
+    };
+
+    const gameId = parseInt(game.split(':')[0].split(' ')[1]);
+    const parts = game.slice(game.indexOf(': ') + 2).split(';');
+
+    for (const part of parts) {
+      const cubePulls = part.split(', ');
+
+      for (const cubePull of cubePulls) {
+        const cubePullParts = cubePull.trim().split(' ');
+        const number = parseInt(cubePullParts[0]);
+        const color = cubePullParts[1];
+
+        if (number > minCubes[color]) {
+          minCubes[color] = number;
+        }
+      }
+    }
+
+    console.log(minCubes.red * minCubes.green * minCubes.blue);
+    console.log('################');
+
+    total += minCubes.red * minCubes.green * minCubes.blue;
+  }
+
+  return total;
 }
 
 const answer = await day2b();
