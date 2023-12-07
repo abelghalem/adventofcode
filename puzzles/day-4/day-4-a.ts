0e54e2366b601f839e8cc5027548ccb027ac2a52
Abdelmounaim Belghalem <abelghalem@greenspector.com>
2023-12-07 12:23:00 +0100

    feat(day4): add code for day 4

--- puzzles/day-4/day-4-a.ts
+++ puzzles/day-4/day-4-a.ts
@@ -1,9 +1,47 @@
-import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import { readData } from '../../shared.ts';
+
+type Card = {
+  id: number;
+  winningNumbers: number[];
+  playedNumbers: number[];
+};
 
 export async function day4a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const cards: Array<Card> = [];
+  for (const line of data) {
+    const [cardNumber, numbers] = line.split(': ');
+    const [winningNumbers, playedNumbers] = numbers.split(' | ');
+    const id = parseInt(cardNumber.split(' ')[1]);
+
+    cards.push({
+      id,
+      winningNumbers: winningNumbers
+        .split(' ')
+        .map((n) => parseInt(n))
+        .filter((n) => Number.isInteger(n)),
+      playedNumbers: playedNumbers
+        .split(' ')
+        .map((n) => parseInt(n))
+        .filter((n) => Number.isInteger(n)),
+    });
+  }
+
+  let total = 0;
+
+  for (const card of cards) {
+    const validNumbers = card.playedNumbers.filter((n) =>
+      card.winningNumbers.includes(n)
+    );
+
+    if (validNumbers.length) {
+      total += Math.pow(2, validNumbers.length - 1);
+    }
+  }
+
+  return total;
 }
 
 const answer = await day4a();
--- puzzles/day-4/day-4-b.ts
+++ puzzles/day-4/day-4-b.ts
@@ -1,9 +1,59 @@
-import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import { readData } from '../../shared.ts';
+
+type Card = {
+  id: number;
+  winningNumbers: number[];
+  playedNumbers: number[];
+  count: number;
+};
 
 export async function day4b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const cards: Array<Card> = [];
+
+  for (const line of data) {
+    const [cardNumber, numbers] = line.split(': ');
+    const [winningNumbers, playedNumbers] = numbers.split(' | ');
+    const id = parseInt(cardNumber.split(' ')[1]);
+
+    cards.push({
+      id,
+      winningNumbers: winningNumbers
+        .split(' ')
+        .map((n) => parseInt(n))
+        .filter((n) => Number.isInteger(n)),
+      playedNumbers: playedNumbers
+        .split(' ')
+        .map((n) => parseInt(n))
+        .filter((n) => Number.isInteger(n)),
+      count: 1,
+    });
+  }
+
+  let total = 0;
+
+  for (const cardIndex in cards) {
+    const card = cards[cardIndex];
+
+    const validNumbers = card.playedNumbers.filter((n) =>
+      card.winningNumbers.includes(n)
+    );
+
+    if (validNumbers.length) {
+      for (let j = 0; j < card.count; j++) {
+        for (let i = 1; i <= validNumbers.length; i++) {
+          if (parseInt(cardIndex) === 0) {
+            console.log(`adding count to card ${parseInt(cardIndex) + i + 1}`);
+          }
+          cards[parseInt(cardIndex) + i].count++;
+        }
+      }
+    }
+  }
+
+  return cards.reduce((total, card) => total + card.count, 0);
 }
 
 const answer = await day4b();
